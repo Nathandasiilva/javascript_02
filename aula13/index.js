be7b17141185b31502475ebbfd1819e2e6df51b1
Nathan Da Silva <117369146+Nathandasiilva@users.noreply.github.com>
2023-06-30 13:22:29 -0300

    Update

---
+++ aula13/index.js
@@ -0,0 +1,14 @@
+//let umaString = "Um \"texto\""; 
+// A barra invertida ela funciona como scap para o codigo funcionar
+
+// 
+/*
+let umaString = "Um texto"
+
+console.log(umaString.concat(" em um lindo dia"));
+console.log(umaString + " em um lindo dia")
+console.log(`${umaString} em um lindo dia`) */
+
+let umaString = "Um texto";
+
+console.log(umaString.indexOf('um'));
