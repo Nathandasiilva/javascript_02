7efb5918827ad51b026e5971370dc4a66e6f8bd2
Nathan Da Silva <117369146+Nathandasiilva@users.noreply.github.com>
2023-06-30 16:19:58 -0300

    Update

---
+++ aula15/index.js
@@ -0,0 +1,11 @@
+//let num1 = 9;
+
+console.log(!!(100/ 0));
+
+//console.log(num1 ** 0.5);
+// let num2 = Math.floor(num1); Arredonda para baixo
+//let num1 = Math.ceil(num1); Arredonda para cima
+//let num1 = Math.round(num1); Arredonda para o mais proximo, da metade para cima e do 49 pra biaxo
+//console.log(Math.max(1,2,5,-5,2222,5,-5555));
+//console.log(Math.min(1,2,5,-5,2222,5,-5555));
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
