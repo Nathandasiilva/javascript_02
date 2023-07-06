fa822a1dc6ce14ed80b7a7b193edacc46a27776d
Nathan Da Silva <117369146+Nathandasiilva@users.noreply.github.com>
2023-07-05 21:09:33 -0300

    Update

---
+++ aula16/index.js
@@ -0,0 +1,35 @@
+const alunos = ["Luiz", "Maria", "João"];
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+
+
+
+
+
+//alunos.push('Luiza');
+//alunos.push('Eduardo');
+
+//console.log(alunos.slice(0, -1)); fatia
+
+//console.log(alunos[50]);; acessa um que não esta na memoria
+
+//delete alunos[1]; deleta sem mecher no idece, mas fica um buraco
+//console.log(alunos[1]);
+
+//const removido = alunos.shift();remove do inicio
+//const removido = alunos.pop(); remove do final
+
+
+//alunos.unshift('luiza') Adciona no começo
+//alunos.push('Alirdo'); adicionando no fim
+//alunos [alunos.length] = 'Fabio'; adicionando no fim
+
+//alunos[0] = 'Eduardo'; altera
+//alunos[3] = 'Eloiza'; adionando
+
+
+//console.log(alunos);
+//console.log(alunos[0]); acessando
+//console.log(alunos[2]);
---
+++ aula17/index.js
@@ -0,0 +1,21 @@
+const raiz = function(n) {
+ return n ** 0.5;
+};
+console.log(raiz(9));
+console.log(raiz(3));
+
+
+//function soma (4, 2) {
+//console.log(soma(2, 2));
+//    const resultado = x + y;
+//    return resultado;
+  
+
+//function saudacao(nome){
+// console.log(`Bom dia ${nome}!`);
+// return 12345;
+//}
+
+
+//const variavel = saudacao('Nathan');
+// console.log(variavel);
