0d6b8eddf4e0c1038448de76e40adeb595a0ffc3
Nathan Da Silva <117369146+Nathandasiilva@users.noreply.github.com>
2023-07-12 19:59:22 -0300

    Update

---
+++ aula21/index.js
@@ -0,0 +1,12 @@
+/* 
+Operadores de compração
+> maior que
+>= maior ou igual a
+< menor que
+<= menor ou igual a 
+== igualdade (valor)
+=== igualdade estrita (valor e tipo)
+!= diferente
+!== diferente estrito (valor e tipo)
+*/
+ 
