0eca94437b1134c86793277705feba23f13eba25
Nathan Da Silva <117369146+Nathandasiilva@users.noreply.github.com>
2023-07-18 21:19:56 -0300

    Update

---
+++ aula27/index.js
@@ -0,0 +1,14 @@
+// Condição ? 'Valor para verdadeiro'  :  'Valor para falso';
+
+const pontuacaoUsuario = 1000;
+const nivelDeUsaurio = pontuacaoUsuario >= 1000 ? 'Usuario VIP' : 'Usuario normal';
+ 
+console.log(nivelDeUsaurio);
+
+
+//if (pontuacaoUsuario >= 1000) {
+//    console.log('Usuario VIP');
+//}
+//else {
+//    console.log('Usuario normal')
+//}
