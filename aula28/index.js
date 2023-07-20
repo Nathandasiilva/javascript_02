e9ca996bdd15f438fe236da15b570acb15f4f554
Nathan Da Silva <117369146+Nathandasiilva@users.noreply.github.com>
2023-07-20 19:55:04 -0300

    Update

--- aula28/index.js
+++ aula28/index.js
@@ -1,7 +1,32 @@
+function zeroAEsquerda (num) {
+    return num >= 10 ? num : `0${num}`;
+}
+
+function formataData(data) {
+const dia = zeroAEsquerda (data.getDate());
+const mes = zeroAEsquerda (data.getMonth() + 1);
+const ano = zeroAEsquerda (data.getFullYear());
+const hora = zeroAEsquerda (data.getHours());
+const min = zeroAEsquerda (data.getMinutes());
+const seg = zeroAEsquerda (data.getSeconds());
+
+return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`;
+}
+
+const data = new Date();
+const dataBrasil = formataData(data);
+console.log(dataBrasil); 
+
+
+
+
+
+
 //const data = new Date(2020, 3,); // ano, mes, dia, hora, minuto, segundos, ms
-const data = new Date('2020-10-28 20:20:20');
+/*
+const data = new Date(1689889193408);
 console.log('Dia', data.getDate());
-console.log('Mes', data.getMonth());
+console.log('Mes', data.getMonth() + 1);
 console.log('Ano', data.getFullYear());
 console.log('Hora', data.getHours());
 console.log('Min', data.getMinutes());
@@ -9,3 +34,5 @@ console.log('Seg', data.getSeconds());
 console.log('Ms', data.getMilliseconds());
 console.log('Dia semana', data.getDay());
 console.log(data.toString()); 
+console.log(Date.now());
+*/
---
+++ aula29/index.js
@@ -0,0 +1,85 @@
+function getDiaSemanaTexto (diaSemana) {
+    let diaSemanaTexto;
+
+    switch(diaSemana) {
+    case 0:
+      diaSemanaTexto = 'Domingo';
+    return diaSemanaTexto;
+    case 1:
+      diaSemanaTexto = 'Segunda';
+    return diaSemanaTexto;
+    case 2:
+      diaSemanaTexto = 'Terça';
+    return diaSemanaTexto;
+    case 3:
+      diaSemanaTexto = 'Quarta';
+    return diaSemanaTexto;
+    case 4:
+      diaSemanaTexto = 'Quinta';
+    return diaSemanaTexto;
+    case 5:
+      diaSemanaTexto = 'Sexta';
+    return diaSemanaTexto;
+    case 6:
+      diaSemanaTexto = 'Sábado';
+    return diaSemanaTexto;
+    default:
+      diaSemanaTexto = '';
+    return diaSemanaTexto;
+
+}
+}
+
+
+const data = new Date('2023-07-20 00:00:00');
+let diaSemana = data.getDay();
+const diaSemanaTexto = getDiaSemanaTexto(diaSemana);
+
+console.log(diaSemana, diaSemanaTexto);
+
+/*
+switch(diaSemana) {
+    case 0:
+    diaSemanaTexto = 'Domingo';
+    break;
+case 1:
+    diaSemanaTexto = 'Segunda';
+    break;
+case 2:
+    diaSemanaTexto = 'Terça';
+    break;
+case 3:
+    diaSemanaTexto = 'Quarta';
+    break;
+case 4:
+    diaSemanaTexto = 'Quinta';
+    break;
+case 5:
+    diaSemanaTexto = 'Sexta';
+    break;
+case 6:
+    diaSemanaTexto = 'Sábado';
+    break;
+}
+
+ */
+
+
+//if (diaSemana === 0){
+//    diaSemanaTexto = 'Domingo';
+//}  else if (diaSemana === 1){
+//    diaSemanaTexto = 'Segunda';
+//}  else if (diaSemana === 1){
+//    diaSemanaTexto = 'Terça';
+// } else if (diaSemana === 1){
+//   diaSemanaTexto = 'Quarta';
+//} else if (diaSemana === 1){
+//    diaSemanaTexto = 'Quinta';
+// } else if (diaSemana === 1){
+//    diaSemanaTexto = 'Sexta';
+// } else if (diaSemana === 1){
+//    diaSemanaTexto = 'Sábado';
+// } else {
+//    diaSemanaTexto = ''; 
+// }
+
