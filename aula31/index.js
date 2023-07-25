a081b554fa6172b390f8c5983de179b601aa88cb
Nathan Da Silva <117369146+Nathandasiilva@users.noreply.github.com>
2023-07-25 19:21:26 -0300

    Update

---
+++ aula31/index.js
@@ -0,0 +1,27 @@
+const verdadeira = true;
+
+// Let tem espaço de bloco {.... Bloco}
+// Var só tem, escopo de função
+
+//let nome = 'Nathan';
+//var nome2 = 'Nathan';
+//console.log(nome, nome2);
+
+
+//if (verdadeira){
+//let nome = 'Nathan';
+///var nome2 = 'silva';
+
+//console.log(nome, nome2);
+
+//if (verdadeira){
+//let nome = 'Nathan';
+//var nome2 = 'Silva';
+//console.log(nome, nome2);
+//}
+//}
+
+
+
+let sobrenome = 'Silva';
+console.log(sobrenome);
