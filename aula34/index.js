0579d5e6c73ffb92635df943eaea7fd5fe2ff808
Nathan Da Silva <117369146+Nathandasiilva@users.noreply.github.com>
2023-08-01 19:31:45 -0300

    Update

---
+++ aula34/index.js
@@ -0,0 +1,10 @@
+const frutas = ['Maça', 'Bnana', 'Uva']; 
+
+for (let i = 0; i < frutas.length; i++){
+console.log(i);    
+}
+
+//for (let i = 0; i <= 5; i++) {    
+//const par = i % 2 === 0 ? 'par' : 'Impar';
+//console.log(i, par);
+//}
