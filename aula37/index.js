20940524cfb72a2d1c76b954f5aeba9239acb1fe
Nathan Da Silva <117369146+Nathandasiilva@users.noreply.github.com>
2023-08-03 20:17:37 -0300

    Update

---
+++ aula37/index.js
@@ -0,0 +1,38 @@
+//
+
+
+
+
+
+
+// For classico - Geralmente com interaveis (array ou strings)
+// For in - Retorna o indece ou chaves (strings, array ou objeto)
+// For of - Retorna o valor em si (iteraveis, array ou strings)
+
+
+
+/*
+const nome = ['Nathan da Silva', 'Daniel gomes', 'Ygor jumes'];
+
+for (let i =0; i < nome.length; i++) {
+    console.log(nome[i]);
+}
+
+console.log('####');
+
+for (let i in nome) {
+    console.log(nome[1]);
+}
+
+console.log('####');
+ 
+for (let valor of nome) {
+    console.log(valor);
+}
+
+console.log('####');
+
+nome.forEach(function(valor, indice, array) {
+   console.log(valor, indice, array);
+});
+*/
